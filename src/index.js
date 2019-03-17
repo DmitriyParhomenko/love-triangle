6cfd6c82b440f9b9fb0e2f80d460300e61fc394a
MacOS <macos@MacOSs-MacBook-Pro.local>
2019-03-17 23:10:25 +0300

    feat: task love-triangle

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(arr) {
+  return arr.reduce(((count, current, index, arr ) => (arr[index] !== index + 1 && arr[arr[arr[index] - 1] - 1] === index + 1 ) ? ++count : count), 0) / 3;
 };
